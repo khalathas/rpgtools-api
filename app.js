47a617dad80f92c6f6bc286d05bb5a30dc6d7926
Justin McClellan <khalathas@gmail.com>
2024-02-20 18:31:14 -0500

    config setup finally works, need to fix passing db connection to routes next

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ const configMethods = require('./config/config');
 let db;
 async function setupDb() {
     // check if config exists
-    const hasConfig = async () => await configMethods.checkConfig();
+    const hasConfig = await configMethods.checkConfig();
+    console.log("hasConfig value: ",hasConfig);
 
     // if exists, use file, else create it and use the object returned from the create method
-    const config = async () => hasConfig ? await configMethods.getConfig() : await configMethods.createConfig();
+    const config = hasConfig ? await configMethods.getConfig() : await configMethods.createConfig();
+    console.log("config value: ",config);
 
     // set up db connector with values from config
     db = mysql.createConnection({
